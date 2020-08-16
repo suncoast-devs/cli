94766a37478a382b9c530faf53c28c5a9ac2eb5d
Gavin Stark <gavin@gstark.com>
2020-08-15 23:56:36 -0400

    Update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ switch (arguments[0]) {
       const message = arguments[2]
 
       if (process.platform === 'win32') {
-        exec(`${__dirname}/.bin/snoretoast -silent "${message}" -t "${appName}"`)
+        exec(`${__dirname}/.bin/snoretoast -silent -m "${message}" -t "${appName}"`)
       } else {
         exec(
           `${__dirname}/.bin/terminal-notifier.app/Contents/MacOS/terminal-notifier -message "${message}" -title "${appName}"`
