4202b2b421427535ca8b79c81585fd5aa67eb0b0
Gavin Stark <gavin@gstark.com>
2020-10-01 14:08:17 -0400

    Adds ability to add/remove packages - and refactors downloading code

--- warpGate.js
+++ warpGate.js
@@ -2,6 +2,8 @@ const axios = require('axios')
 const fs = require('fs')
 const path = require('path')
 const YAML = require('yaml')
+const { exec } = require('./utils')
+const childProcess = require('child_process')
 
 async function getManifest(template) {
   try {
@@ -12,6 +14,17 @@ async function getManifest(template) {
     return YAML.parse(data)
   } catch {
     return { files: [], checks: { files: [] } }
+  }
+}
+
+function executeCommand(command, args) {
+  const { stderr, status } = childProcess.spawnSync(command, args)
+
+  if (status !== 0) {
+    console.log('Error:')
+    console.log(stderr)
+  } else {
+    console.log('Done!')
   }
 }
 
@@ -28,32 +41,72 @@ async function warpGate(template) {
     return
   }
 
-  const files = manifest.files
+  const { files, packages } = manifest
+
   if (files.length === 0) {
     console.error(`There were no files found for the template: '${template}'`)
     return
   }
 
-  files.forEach(file => {
-    const dirName = path.dirname(file)
-
-    const url = `https://raw.githubusercontent.com/suncoast-devs/warp-gate/trunk/${template}/${file}`
-
-    fs.mkdirSync(dirName, { recursive: true })
-
+  const filePromises = files.map(file =>
     axios({
       method: 'get',
-      url: url,
+      url: `https://raw.githubusercontent.com/suncoast-devs/warp-gate/trunk/${template}/${file}`,
       responseType: 'stream',
     })
-      .then(function (response) {
-        response.data.pipe(fs.createWriteStream(file))
-        console.log(`Downloaded: ${file}`)
-      })
-      .catch(function (error) {
-        console.error(`Failed to download: ${file}`)
-      })
+  )
+
+  const fileResults = await Promise.all(filePromises)
+
+  fileResults.forEach((axiosResult, index) => {
+    const file = files[index]
+
+    if (axiosResult.status === 200) {
+      fs.mkdirSync(path.dirname(file), { recursive: true })
+
+      axiosResult.data.pipe(fs.createWriteStream(file))
+      console.log(`Downloaded: ${file}`)
+    } else {
+      console.error(`Failed to download: ${file}`)
+    }
   })
+
+  if (packages) {
+    const { add, remove } = packages
+
+    const isYarn = fs.existsSync('yarn.lock')
+    const isNpm = fs.existsSync('package-lock.json')
+
+    Array(add)
+      .filter(Boolean)
+      .flat()
+      .forEach(addPackage => {
+        if (isYarn) {
+          process.stdout.write(`yarn: adding package ${addPackage} => `)
+          executeCommand('yarn', ['add', addPackage])
+        } else {
+          if (isNpm) {
+            process.stdout.write(`npm: adding package ${addPackage} => `)
+            executeCommand('npm', ['install', addPackage])
+          }
+        }
+      })
+
+    Array(remove)
+      .filter(Boolean)
+      .flat()
+      .forEach(removePackage => {
+        if (isYarn) {
+          console.log(`yarn: adding package ${removePackage}`)
+          executeCommand('yarn', ['remove', removePackage])
+        } else {
+          if (isNpm) {
+            console.log(`npm: adding package ${removePackage}`)
+            executeCommand('npm', ['uninstall', removePackage])
+          }
+        }
+      })
+  }
 }
 
 exports.warpGate = warpGate
